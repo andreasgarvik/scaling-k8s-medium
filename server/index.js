7395e718a3bf5e664bb0ee5e5b7faa1bd256cc99
Andreas Garvik <andreas.garvik13@gmail.com>
2020-03-11 18:03:29 +0100

    :sparkles:

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ const app = require('express')()
 
 app.get('/', (req, res) => {
 	const start = Date.now()
-	for (let i = 0; i <= 1000000000; i++) {
-		let dummy = Math.log(i + 1)
+	for (let i = 0; i <= 5000000000; i++) {
+		Math.log(i + 1)
 	}
 	const time = Date.now() - start
 	res.status(200).json(time)
 })
 
-app.listen(8080)
+const PORT = process.env.PORT || 8080
+app.listen(PORT)
